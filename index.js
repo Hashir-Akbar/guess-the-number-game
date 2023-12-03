59a431ada3751ff425ca32ec7ec02b0ea315ed54
Hashir Akbar <hashirakbar69@gmail.com>
2023-12-04 01:02:54 +0500

    added functionality of arrowup and down

--- index.js
+++ index.js
@@ -1,93 +1,108 @@
 $(document).ready(function () {
-   $(".fromToNum").hide();
-   $(".guess-btn").hide()
-   $(".restart-btn").hide()
-   $(".input").prop("disabled", true);
-   $(".input").val("");
-   $(".gif").removeAttr("src")
+	$(".fromToNum").hide();
+	$(".guess-btn").hide();
+	$(".restart-btn").hide();
+	$(".input").prop("disabled", true);
+	$(".input").val("");
+	$(".gif").removeAttr("src");
 
-   let audio = new Audio("/audio/firework-show-short-64657.mp3");
+	let audio = new Audio("/audio/firework-show-short-64657.mp3");
+	let regExp = new RegExp("^([1-9]|1[0-9]|2[0-5])$");
 
-   let regExp = new RegExp("^([1-9]|1[0-9]|2[0-5])$");
+	function arrows() {
+		let i = 0;
+		$(`[data-arrow='up']`).on("click", () => {
+			if (i >= 0 && i < 25) {
+				i++;
+				$(".input").val(i);
+			}
+		});
+		$(`[data-arrow='down']`).on("click", () => {
+			if (i > 1 && i <= 25) {
+				i--;
+				$(".input").val(i);
+			}
+		});
+	}
+	function guessLowSms() {
+		let guessLow = [
+			"Guess Low",
+			"hehe, guess more low",
+			"Ehhh, not reached guess low",
+			"More down yet, guess low",
+			"Guess low yet",
+		];
 
+		let i = Math.floor(Math.random(guessLow.length) * guessLow.length);
+		return guessLow[i];
+	}
+	function guessHighSms() {
+		let guessHigh = [
+			"Guess High",
+			"Guess more, high to the sky",
+			"not yet, guess high",
+			"More high, like an eagle ",
+			"Guess high, ..EHH",
+		];
 
+		let i = Math.floor(Math.random(guessHigh.length) * guessHigh.length);
+		return guessHigh[i];
+	}
 
-   let randomNum = randomNumber();
+	let randomNum;
+	function startBtn() {
+		$(".start").on("click", () => {
+			$(".fromToNum").show();
+			$(".guess-btn").show();
+			$(".start").hide();
+			$(".input").removeAttr("disabled");
+			if ($(".start").is(":hidden")) {
+				arrows();
+			} else {
+			}
+			randomNum = randomNumber();
+			randomNum;
+		});
+	}
+	startBtn();
 
-   function startBtn() {
+	$(".restart-btn").on("click", () => {
+		$(".restart-btn").hide();
+		$(".start").show();
+		$(".input").val("");
+		$(".check").html("");
+		$(".gif").removeAttr("src");
+		audio.pause();
+	});
 
-      $(".start").on("click", () => {
-         $(".fromToNum").show();
-         $(".guess-btn").show()
-         $(".start").hide()
-         $(".input").removeAttr("disabled");
-         randomNum;
+	function randomNumber() {
+		let randomNum = Math.floor(Math.random() * 25) + 1;
+		return randomNum;
+	}
 
-      })
-   }
-   startBtn()
+	$(".guess-btn").on("click", () => {
+		if ($(".input").val().match(regExp)) {
+			$(".check").css("font-size", "1.4rem");
+			let inputVal = $(".input").val();
+			if (inputVal == randomNum) {
+				$(".check").html("Congrats, you won");
+				$(".input").prop("disabled", true);
+				$(".restart-btn").show();
+				$(".guess-btn").hide();
+				$(".gif").attr("src", "img/firework.gif");
+				audio.currentTime = 0;
+				audio.play();
+			} else if (inputVal == "") {
+				$(".check").html("");
+			} else if (inputVal > randomNum) {
+				$(".check").html(guessLowSms());
+			} else if (inputVal < randomNum) {
+				$(".check").html(guessHighSms());
+			}
+		} else {
+			$(".check").html("Only input numbers between 1-25");
 
-   $(".restart-btn").on("click", () => {
-      $(".restart-btn").hide()
-      $(".start").show()
-      $(".input").val("");
-      $(".check").html("");
-   $(".gif").removeAttr("src")
-   audio.pause()
-
-
-
-
-      return startBtn()
-
-   })
-
-   function randomNumber() {
-      let randomNum = Math.floor(Math.random() * 25) + 1;
-      return randomNum;
-   }
-
-
-
-
-   $(".guess-btn").on("click", () => {
-
-      if ($(".input").val().match(regExp)) {
-
-         $(".check").css("font-size", "1.4rem");
-         let inputVal = $(".input").val();
-         if (inputVal == randomNum) {
-            $(".check").html("Congrats, you won");
-            $(".input").prop("disabled", true);
-            $(".restart-btn").show()
-            $(".guess-btn").hide()
-            $(".gif").attr('src', "img/firework.gif")
-            audio.play()
-
-
-
-         } else if (inputVal == "") {
-            $(".check").html("");
-
-         } else if (inputVal > randomNum) {
-            $(".check").html("Guess Low");
-
-         }
-         else if (inputVal < randomNum) {
-            $(".check").html("Guess high");
-
-         }
-
-      }
-      else {
-         $(".check").html("Only input numbers between 1-25");
-
-         $(".check").css("font-size", "1.9rem");
-      }
-
-
-   })
-
-
-
+			$(".check").css("font-size", "1.9rem");
+		}
+	});
 });
